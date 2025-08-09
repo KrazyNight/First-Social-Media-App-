c8c13610b150bf3fbaf31e09ea5ea35f08445d8f
Erik Nieto Reynoso <erik.nieto101@gmail.com>
2025-08-09 14:58:30 -0700

    Widgets Part 2

--- app/page.tsx
+++ app/page.tsx
@@ -11,8 +11,8 @@ export default function Home() {
       flex
       
       ">
-        {/* <Sidebar />
-        <PostFeed /> */}
+        <Sidebar />
+        <PostFeed />
         <Widgets />
 
       </div>
